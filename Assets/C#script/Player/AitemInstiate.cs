4ae545dff849f703ff5e253c6edb7085a0f82fbf
Mori Kazuma <kkam0707@gmail.com>
2017-10-12 18:46:09 +0900

    Commit

---
+++ Assets/C#script/Player/AitemInstiate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AitemInstiate : MonoBehaviour {
+
+    public GameObject Wall;
+     void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(Wall, new Vector3(transform.position.x, transform.position.y, transform.position.z + 5), new Quaternion(0,0,0,0));
+        }
+    }
+}
---
+++ Assets/C#script/Player/PlayerMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    #region variable
+    private CharacterController Player;
+    private Vector3 Velocity;
+    #endregion
+    #region Event
+    // Use this for initialization
+    void Start()
+    {
+        Player = GetComponent<CharacterController>();
+        Velocity = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Player.isGrounded)
+        {
+            Velocity.x = Input.GetAxis("Horizontal");
+            Velocity.y = 0;
+            Velocity.z = Input.GetAxis("Vertical");
+        }
+        Velocity.y += Physics.gravity.y * Time.deltaTime;
+        Player.Move(Velocity * Time.deltaTime);
+
+    }
+    #endregion
+
+}

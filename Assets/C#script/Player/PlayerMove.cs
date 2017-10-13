d54cbae43b86d51bd45f9ac9fab7edea09ff078b
Mori Kazuma <kkam0707@gmail.com>
2017-10-13 12:29:01 +0900

    Add JoyStick(Move Player)

--- Assets/C#script/Player/PlayerMove.cs
+++ Assets/C#script/Player/PlayerMove.cs
@@ -4,28 +4,41 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
     #region variable
-    private CharacterController Player;
-    private Vector3 Velocity;
+    //private CharacterController Player;
+    //private Vector3 Velocity;
+
+    [SerializeField]
+    private Joystick _joystick=null;
+
+    //移動速度
+    private const float SPEED = 0.1f;
+
     #endregion
     #region Event
     // Use this for initialization
     void Start()
     {
-        Player = GetComponent<CharacterController>();
-        Velocity = Vector3.zero;
+        //Player = GetComponent<CharacterController>();
+        //Velocity = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Player.isGrounded)
-        {
-            Velocity.x = Input.GetAxis("Horizontal");
-            Velocity.y = 0;
-            Velocity.z = Input.GetAxis("Vertical");
-        }
-        Velocity.y += Physics.gravity.y * Time.deltaTime;
-        Player.Move(Velocity * Time.deltaTime);
+        Vector3 pos = transform.position;
+        pos.x += _joystick.Position.x * SPEED;
+        pos.z += _joystick.Position.y * SPEED;
+
+        transform.position = pos;
+
+        //if(Player.isGrounded)
+        //{
+        //    Velocity.x = Input.GetAxis("Horizontal");
+        //    Velocity.y = 0;
+        //    Velocity.z = Input.GetAxis("Vertical");
+        //}
+        //Velocity.y += Physics.gravity.y * Time.deltaTime;
+        //Player.Move(Velocity * Time.deltaTime);
 
     }
     #endregion
